5ce232a1a97ef85ba1636e98a7f51061f6d7e9e5
sjwconway <sjwconway@hotmail.com>
2019-08-22 00:40:19 +1000

    added semantic ui and basic files

---
+++ src/Components/Pages/Dashboard.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import DashboardSidebar from '../DasboardSidebar';
+class Dashboard extends Component {
+    render() {
+        return (
+            <>
+            <div>
+                <h1>Dashboard</h1>
+            </div>
+            <DashboardSidebar />
+            </>
+        )
+    }
+};
+
+export default Dashboard;
